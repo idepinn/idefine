341bc438863e9a37190c7196fd170292f6f47fec
oneagustiranda <56112959+oneagustiranda@users.noreply.github.com>
2021-10-25 08:49:32 +0700

    add post idea prototype view

--- idefine-web/resources/views/home.blade.php
+++ idefine-web/resources/views/home.blade.php
@@ -8,6 +8,16 @@
         </div>
         <div class="col-md-5">
             <h4>Start with idea</h4>
+            <hr>
+            <h5 class="text-center mb-5">People's Idea you might Interest</h5>
+            @foreach ($posts as $home)
+            <article class="card p-3 mb-3 shadow-sm rounded-pill border-top">
+                <h6>
+                    <a href="/idea/{{ $home["slug"] }}">{{ $home["title"] }} - {{ $home["author"] }}</a>
+                </h6>
+                <span>{{ $home["category"] }}</span>
+            </article>
+            @endforeach
         </div>
         <div class="col-md-3">
             <h4>My Connection</h4>
--- idefine-web/routes/web.php
+++ idefine-web/routes/web.php
@@ -13,11 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
+$idea_posts = [
+    [
+        "title" => "Find group of farmer",
+        "slug" => "find-group-of-farmer",
+        "author" => "Frilyan Juanda",
+        "category" => "Farmer"
+    ],
+    [
+        "title" => "Make financial Partner",
+        "slug" => "make-financial-partner",
+        "author" => "Ervin Ahmad",
+        "category" => "Fintech"
+    ]
+];
     return view('home', [
         "title" => "Home",
         "name" => "One Agustiranda",
-        "image" => "profile.jpg"
+        "image" => "profile.jpg",
+        "posts" => $idea_posts
     ]);
 });
 
@@ -30,6 +47,43 @@ Route::get('/connection', function () {
 Route::get('/idea', function () {
     return view('idea', [
         "title" => "Idea"
+    ]);
+});
+
+// Single page from idea
+Route::get('/idea/{slug}', function ($slug) {
+    $idea_posts = [
+        [
+            "title" => "Find group of farmer",
+            "slug" => "find-group-of-farmer",
+            "author" => "Frilyan Juanda",
+            "category" => "Farmer",
+            "article" => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem.
+                        In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy. Fusce aliquet pede non pede. Suspendisse dapibus lorem pellentesque magna. Integer nulla. Donec blandit feugiat ligula. Donec hendrerit, felis et imperdiet euismod, purus ipsum pretium metus, in lacinia nulla nisl eget sapien.
+                        Donec ut est in lectus consequat consequat. Etiam eget dui. Aliquam erat volutpat. Sed at lorem in nunc porta tristique. Proin nec augue. Quisque aliquam tempor magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc ac magna. Maecenas odio dolor, vulputate vel, auctor ac, accumsan id, felis. Pellentesque cursus sagittis felis.
+                        "
+        ],
+        [
+            "title" => "Make financial Partner",
+            "slug" => "make-financial-partner",
+            "author" => "Ervin Ahmad",
+            "category" => "Fintech",
+            "article" => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend.
+                        Ut nonummy. Fusce aliquet pede non pede. Suspendisse dapibus lorem pellentesque magna. Integer nulla. Donec blandit feugiat ligula. Donec hendrerit, felis et imperdiet euismod, purus ipsum pretium metus, in lacinia nulla nisl eget sapien. Donec ut est in lectus consequat consequat. Etiam eget dui. Aliquam erat volutpat. Sed at lorem in nunc porta tristique. Proin nec augue. Quisque aliquam tempor magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc ac magna.
+                        "
+        ]
+    ];
+
+    $new_post = [];
+    foreach($idea_posts as $post){
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+    }
+
+    return view('ideapost',[
+        "title" => "Idea Post",
+        "post" => $new_post
     ]);
 });
 
